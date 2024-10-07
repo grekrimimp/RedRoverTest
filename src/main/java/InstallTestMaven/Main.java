940b21030abdaa0507d94f0975de1a35f473e722
Denis <grekrimimp@gmail.com>
2024-10-07 15:07:11 +0300

    test3-3

--- src/main/java/InstallTestMaven/Main.java
+++ src/main/java/InstallTestMaven/Main.java
@@ -12,10 +12,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-
-
         }
-
         First_class2 pec = new First_class2();
         pec.pec();
     }
